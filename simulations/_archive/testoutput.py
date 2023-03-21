5af7440bc46a48d0d117065597031e108fc458bd
Niklas Brake <niklas.brake@mail.mcgill.ca>
2023-03-21 14:31:38 -0400

    Merged publication code and moved older files into archive

--- simulations/_archive/testoutput.py
+++
@@ -1,6 +0,0 @@
-import sys
-print('hi');
-print('E:/Research_Projects/004_Propofol/Modelling/neuron_simulations/code');
-
-print('hi');
-print('E:/Research_Projects/0sdf4_Propofol/Modelling/neuron_simulations/code');
