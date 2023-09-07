56013cf3707ab80c1cf4f6586c6acd5d0345af27
Niklas Brake <niklas.brake@mail.mcgill.ca>
2023-09-07 15:09:37 -0400

    preliminary network modelling

--- beluga_simulations/EI_network/run_simulation.m
+++ beluga_simulations/EI_network/run_simulation.m
@@ -13,13 +13,14 @@ network.tmax = 10e3; % 2 seconds
 
 [ids,ts,ei,P,trueL] = run_EInetwork(N,0);
 network_simulation_beluga.save_presynaptic_network(ids,ts,ei,N,network.spikingFile)
-% network = network.compute_presynaptic_correlations(network.spikingFile)
-% network.embed_presyanptic_neurons;
 
-x1 = P(:,2);
-y1 = P(:,1)+pi/2;
-data = [(1:N)'-1,x1(:),y1(:)];
-dlmwrite(fullfile(network.preNetwork,'UMAP_embedding.csv'),data,'precision','%.4f');
+network = network.compute_presynaptic_correlations(network.spikingFile)
+network.embed_presyanptic_neurons;
+
+% x1 = P(:,2);
+% y1 = P(:,1)+pi/2;
+% data = [(1:N)'-1,x1(:),y1(:)];
+% dlmwrite(fullfile(network.preNetwork,'UMAP_embedding.csv'),data,'precision','%.4f');
 
 % Place syanpse optimally (input between 0 and 1, with 1 optimal and 0 random)
 network = network.form_connections(1);
